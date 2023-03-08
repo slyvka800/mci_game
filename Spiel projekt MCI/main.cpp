f93d1bd7def7deb1526a3d80eb736e1b677170e3
slyvka800 <p.a.slyvka@gmail.com>
2023-03-08 11:38:10 +0100

    Initial Commit

---
+++ Spiel projekt MCI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Spiel projekt MCI
+//
+//  Created by Павло Сливка on 08.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
